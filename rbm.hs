49c658d2af19f554cadfae47e6e6bb139af50a6e
Bill Casarin <billcasarin@gmail.com>
2010-02-23 18:33:19 -0500

    Its called restricted for a reason, finish reading the paper D:

--- rbm.hs
+++ rbm.hs
@@ -5,36 +5,19 @@ import Numeric.LinearAlgebra.Instances
 -- L: visible to visible
 -- J: hidden to hidden
 
-type Matrix' = M.Matrix Double
+type FType = Double
+type Probability = FType
+type Matrix' = M.Matrix FType
 
 type Visible = Matrix'
 type Hidden = Matrix'
 type WParam = Matrix'
 type LParam = Matrix'
 type JParam = Matrix'
-type Parameters = (WParam, LParam, JParam)
 type Output = Matrix'
 
-fst3 :: (a, b, c) -> a
-fst3 (a, b, c) = a
+logistic :: (Floating a) => a -> a
+logistic x = recip (1 + exp (-x))
 
-snd3 :: (a, b, c) -> b
-snd3 (a, b, c) = b
-
-thd3 :: (a, b, c) -> c
-thd3 (a, b, c) = c
-
-energy :: Visible -> Hidden -> Parameters -> Output
-energy v h p = vToV - hToH - vToH
-  where
-    vToV = -0.5 * ((trans v) * lv)
-    hToH = 0.5 * ((trans h) * jh)
-    vToH = (trans v) * wh
-    lv = (snd3 p) * v
-    jh = (thd3 p) * h
-    wh = (fst3 p) * h
---
---assignProb :: Visible -> Hidden -> Double
---assignProb v h = (recip z) * sum $ exp (-(energy v h))
---  where
---    z = sum $ sum 
+energy :: Visible -> Hidden -> WParam -> Output
+energy v h w = negate $ (trans v) * (w * h)
