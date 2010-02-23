624bbe0eeb167e050633466599f641b4476c90b0
Bill Casarin <billcasarin@gmail.com>
2010-02-23 15:21:36 -0500

    Much better, using a linear algebra package to deal with matrix calculations

--- rbm.hs
+++ rbm.hs
@@ -1,31 +1,38 @@
-import Data.List
+import Data.Packed.Matrix as M
+import Numeric.LinearAlgebra.Instances
+
 -- W: visible to hidden
 -- L: visible to visible
 -- J: hidden to hidden
 
+type Matrix' = M.Matrix Double
 
--- Yeah... I'm switching to a matrix lib
-
-type Matrx = [[Double]]
-
-type Visible = Matrix
-type Hidden = Matrix
-type WParam = Matrix
-type LParam = Matrix
-type JParam = Matrix
+type Visible = Matrix'
+type Hidden = Matrix'
+type WParam = Matrix'
+type LParam = Matrix'
+type JParam = Matrix'
 type Parameters = (WParam, LParam, JParam)
-type Output = Matrix
+type Output = Matrix'
+
+fst3 :: (a, b, c) -> a
+fst3 (a, b, c) = a
+
+snd3 :: (a, b, c) -> b
+snd3 (a, b, c) = b
+
+thd3 :: (a, b, c) -> c
+thd3 (a, b, c) = c
 
 energy :: Visible -> Hidden -> Parameters -> Output
-energy v h p = zipWith3 (+) vToV hToH vToH
+energy v h p = vToV - hToH - vToH
   where
-    f = (\x y -> map (*(-0.5)) (x `transpose` y))
-    vToV = f v lv
-    hToH = f h jh
-    vToH = v `transpose` wh
-    lv = zipWith (*) (fst3 p) v
-    jh = zipWith (*) (snd3 p) h
-    wh = zipWith (*) (thd3 p) h
+    vToV = -0.5 * ((trans v) * lv)
+    hToH = 0.5 * ((trans h) * jh)
+    vToH = (trans v) * wh
+    lv = (snd3 p) * h
+    jh = (thd3 p) * h
+    wh = (fst3 p) * h
 --
 --assignProb :: Visible -> Hidden -> Double
 --assignProb v h = (recip z) * sum $ exp (-(energy v h))
